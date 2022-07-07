8409f7517cbb3abfa0299e556d9fd2b9456cb487
Kyle Klink <kyleklink@Air.local>
2022-07-07 11:19:56 -0400

    more helloworld changes

--- HellowWorld/src/HelloWorld.java
+++ HellowWorld/src/HelloWorld.java
@@ -3,6 +3,12 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        String myName = "kyle Klink";
+        System.out.println("My name is " + myName);
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 
 }
